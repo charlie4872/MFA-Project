754404c43ac2beb32ed000f1ef13e22b0dfad98d
charlie4872 <54152271+charlie4872@users.noreply.github.com>
2021-02-16 21:47:40 -0500

    Update Get GLN Users.ps1

--- MFA/Get GLN Users.ps1
+++ MFA/Get GLN Users.ps1
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 (get-aduser -filter * -searchbase "DC=ga,DC=local") | Select-Object SamAccountName, Userprincipalname, Distinguishedname | export-csv .\GLN_Users.csv
+=======
+(get-aduser -filter * -searchbase "DC=ga,DC=local") | Select-Object SamAccountName, Userprincipalname, Distinguishedname | export-csv .\All_GLN_Users.csv
+>>>>>>> parent of 8d8be07 (Update Get GLN Users.ps1)
